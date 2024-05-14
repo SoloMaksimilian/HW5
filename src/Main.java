58410053fc8c4d387109a324dfb46c4ab2c0849d
SoloMaksimilian <mephistosmad@gmail.com>
2024-05-14 19:41:40 +0300

    FIX

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
-        int clientOS = 0;
-        int clientDeviceYear = 2014;
-        if (clientOS == 1) {
-            if (clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию приложения для Android по ссылке");
-            } else {
-                System.out.println("Установите версию приложения для Android по ссылке");
+        int clientOS = 1;
+        int clientDeviceYear = 2016;
+        if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+            {
             }
+        } else if (clientOS != 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         } else {
-            if (clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-            } else {
-                System.out.println("Установите версию приложения для iOS по ссылке");
-            }
-
+            System.out.println("Установите версию приложения для iOS по ссылке");
         }
+
+
+
 
         int year = 1600;
         if (year < 1584) {
@@ -44,6 +44,7 @@ public class Main {
 
 
         int monthNumber = 13;
+        if (monthNumber <=12)
         switch (monthNumber) {
             case 1:
             case 2:
@@ -65,8 +66,7 @@ public class Main {
             case 11:
                 System.out.println("Осень");
                 break;
-            default:
-                System.out.println("Такого месяца нет");
+
         }
 
     }
