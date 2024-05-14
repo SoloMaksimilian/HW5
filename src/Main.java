4e10f27de13030b4f20b1f6b788c801846c90573
SoloMaksimilian <mephistosmad@gmail.com>
2024-05-14 17:45:23 +0300

    EX5

--- src/Main.java
+++ src/Main.java
@@ -21,16 +21,55 @@ public class Main {
         int year = 1600;
         if (year < 1584) {
             System.out.println("Високосный год не изобретен");
-        } else if (year % 4 == 0)
-            {
-                if ((year % 100 != 0) || (year % 400 == 0))
-                {
-                    System.out.println(year + " год является високосный");
-                }
-                else {System.out.println(year + " год не является високосный");}
+        } else if (year % 4 == 0) {
+            if ((year % 100 != 0) || (year % 400 == 0)) {
+                System.out.println(year + " год является високосный");
             } else {
-                System.out.println(year + " год не является високосный");}
+                System.out.println(year + " год не является високосный");
+            }
+        } else {
+            System.out.println(year + " год не является високосный");
+        }
 
+        int deliveryDistance = 66;
+        if (deliveryDistance < 20) {
+            System.out.println("Потребуется 1 день для доставки кары");
+        } else if ((deliveryDistance >= 20) && (deliveryDistance < 60)) {
+            System.out.println("Потребуется 2 дня для доставки кары");
+        } else if ((deliveryDistance >= 60) && (deliveryDistance <= 100)) {
+            System.out.println("Потребуется 3 дня для доставки кары");
+        } else {
+            System.out.println("Доставки нет");
+        }
+
+
+        int monthNumber = 13;
+        switch (monthNumber) {
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Такого месяца нет");
+        }
 
     }
+
+
 }
